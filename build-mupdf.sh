c65f0c095981f7edc230ab926a0eaa084ef1a776
Gustavo Karkow <14905932+karkowg@users.noreply.github.com>
2024-11-10 21:55:12 -0500

    chore: attempt 18/1231823 trying to build binary as release artifacts

--- build-mupdf.sh
+++ build-mupdf.sh
@@ -6,41 +6,33 @@ rm -rf ./build
 mkdir -p build
 mkdir -p bin
 
-(
-    cd build || exit
-    curl "https://mupdf.com/downloads/archive/mupdf-$VERSION-source.tar.gz" -o "mupdf.tar.gz"
-    tar -xf mupdf.tar.gz
-    cd mupdf-$VERSION-source || exit
-    if [[ "$OS" == "linux" ]]; then
-        if [[ "$ARCH" == "x86_64" ]]; then
-            export CC="gcc"
-            export CFLAGS="-m64"
-        elif [[ "$ARCH" == "arm64" ]]; then
-            export CC="aarch64-linux-gnu-gcc"
-        fi
-        # make HAVE_X11=no HAVE_GLUT=no prefix=../../install_${OS}_${ARCH} install
-        make HAVE_X11=no HAVE_GLUT=no prefix=../. install
-    else
-        # if [[ "$ARCH" == "x86_64" ]]; then
-        #     export CC="clang"
-        #     export XCFLAGS="-arch x86_64"
-        #     export XLDFLAGS="-L/usr/local/lib -lX11 -lGL"
-        # elif [[ "$ARCH" == "arm64" ]]; then
-            # export CC="clang"
-            # export XCFLAGS="-arch arm64"
-            # export XLDFLAGS="-L/usr/local/lib -lX11 -lGL"
-        # fi
-        # make HAVE_X11=no HAVE_GLUT=no prefix=../../install_${OS}_${ARCH} install
-        make HAVE_X11=no HAVE_GLUT=no prefix=../. install
+cd build || exit
+curl "https://mupdf.com/downloads/archive/mupdf-$VERSION-source.tar.gz" -o "mupdf.tar.gz"
+tar -xf mupdf.tar.gz
+cd mupdf-$VERSION-source || exit
+if [[ "$OS" == "linux" ]]; then
+    if [[ "$ARCH" == "x86_64" ]]; then
+        export CC="gcc"
+        export CFLAGS="-m64"
+    elif [[ "$ARCH" == "arm64" ]]; then
+        export CC="aarch64-linux-gnu-gcc"
     fi
-)
+    make HAVE_X11=no HAVE_GLUT=no prefix=../. install
+else
+    # if [[ "$ARCH" == "x86_64" ]]; then
+    #     export CC="clang"
+    #     export XCFLAGS="-arch x86_64"
+    #     export XLDFLAGS="-L/usr/local/lib -lX11 -lGL"
+    # elif [[ "$ARCH" == "arm64" ]]; then
+        # export CC="clang"
+        # export XCFLAGS="-arch arm64"
+        # export XLDFLAGS="-L/usr/local/lib -lX11 -lGL"
+    # fi
+    make HAVE_X11=no HAVE_GLUT=no prefix=../. install
+fi
 
+cd ..
 ls -la
-ls -la build
-ls -la build/bin
-# ls -la build/install_${OS}_${ARCH}
-# ls -la build/install_${OS}_${ARCH}/bin
-# ls -la build/install_${OS}_${ARCH}/release
-# cp ./build/${OS}_${ARCH}/mupdf-${VERSION}-source/bin/mutool ./bin/mutool_${OS}_${ARCH}
-cp ./build/bin/mutool ./bin/mutool
+cp ./bin/mutool ../bin/mutool
+cd ..
 ls -la bin
